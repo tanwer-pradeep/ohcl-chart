3083c80d7f58a94fa8e1a57cbcc8e3c1178cd362
tanwer-pradeep <pradeepsinghonline@gmail.com>
2023-03-03 17:31:44 +0530

    updated proxy in package json

--- src/chart/index.js
+++ src/chart/index.js
@@ -1,35 +1,38 @@
 import { Stock } from "@ant-design/plots";
-import { Select } from 'antd';
+import { Select } from "antd";
 import React, { useEffect, useState } from "react";
 import preProcessData from "../utils/dataProcessor";
 import getStartTime from "../utils/TimeFrame";
 
 const selectOptions = [
-  { value: '3y', label: '3 Years' },
-  { value: '1y', label: '1 Year' },
-  { value: '3m', label: '3 months' },
-  { value: '1m', label: '1 month' },
-  { value: '7d', label: '7 day' },
-  { value: '3d', label: '3 day' },
-  { value: '1d', label: '1 day' },
-  { value: '6h', label: '6 hr' },
-  { value: '1h', label: '1 hr' }
-]
+  { value: "3y", label: "3 Years" },
+  { value: "1y", label: "1 Year" },
+  { value: "3m", label: "3 months" },
+  { value: "1m", label: "1 month" },
+  { value: "7d", label: "7 day" },
+  { value: "3d", label: "3 day" },
+  { value: "1d", label: "1 day" },
+  { value: "6h", label: "6 hr" },
+  { value: "1h", label: "1 hr" },
+];
 
 const CandleChart = () => {
   const [apiData, setApiData] = useState([]);
   const [data, setData] = useState([]);
-  const [startTime, setStartTime] = useState(new Date().setHours(new Date().getHours() - 1));
+  const [startTime, setStartTime] = useState(
+    new Date().setHours(new Date().getHours() - 1)
+  );
 
-  const fetchApi = () =>{
-    const baseUrl = "https://api-pub.bitfinex.com/v2/";
+  const fetchApi = () => {
     const pathParams = "candles/trade:5m:tBTCUSD/hist";
     const currentTimeInMilliseconds = new Date().getTime();
-    let queryParams = startTime ? `start=${startTime}&end=${currentTimeInMilliseconds}&limit=330`:`end=${currentTimeInMilliseconds}&limit=330`;
+    let queryParams = startTime
+      ? `start=${startTime}&end=${currentTimeInMilliseconds}&limit=330`
+      : `end=${currentTimeInMilliseconds}&limit=330`;
 
-    fetch(`${baseUrl}${pathParams}?${queryParams}`, {mode: 'cors'})
+    fetch(`${pathParams}?${queryParams}`, { mode: "cors" })
       .then((response) => {
-        return response.json()
+        return response.json();
       })
       .then((res) => {
         setApiData(res);
@@ -37,10 +40,10 @@ const CandleChart = () => {
       .catch((err) => {
         console.log(err);
       });
-  }
+  };
 
   useEffect(() => {
-   fetchApi()
+    fetchApi();
   }, []);
 
   useEffect(() => {
@@ -50,15 +53,15 @@ const CandleChart = () => {
     }
   }, [apiData]);
 
-  useEffect(() =>{
-    if(startTime){
-      fetchApi()
+  useEffect(() => {
+    if (startTime) {
+      fetchApi();
     }
-  }, [startTime])
+  }, [startTime]);
 
-  const handleSelectChange = (event) =>{
-    setStartTime(getStartTime(event))
-  }
+  const handleSelectChange = (event) => {
+    setStartTime(getStartTime(event));
+  };
 
   const config = {
     data,
@@ -66,15 +69,17 @@ const CandleChart = () => {
     yField: ["open", "close", "high", "low"],
   };
 
-  return <>
-  <Stock {...config} />
-  <Select
-      defaultValue="1h"
-      style={{ width: 120 }}
-      options={selectOptions}
-      onChange={(event) => handleSelectChange(event)}
-    />
-  </>;
+  return (
+    <>
+      <Stock {...config} />
+      <Select
+        defaultValue="1h"
+        style={{ width: 120 }}
+        options={selectOptions}
+        onChange={(event) => handleSelectChange(event)}
+      />
+    </>
+  );
 };
 
 export default CandleChart;
--- src/utils/dataProcessor/index.js
+++ src/utils/dataProcessor/index.js
@@ -1,25 +1,25 @@
 const preProcessData = (data) => {
-    let processedData = [];
-    data.forEach((set) => {
-      let trade_date = new Date(set[0]);
-      let close = set[2];
-      let open = set[1];
-      let high = set[3];
-      let low = set[4];
-      let vol = set[5];
-      
-      let eachEntry = {
-        ts_code: "000001.SH",
-        trade_date,
-        close,
-        open,
-        high,
-        low,
-        vol,
-      };
-      processedData.push(eachEntry);
-    });
-    return processedData;
-  };
+  const processedData = [];
+  data.forEach((set) => {
+    const trade_date = new Date(set[0]);
+    const close = set[2];
+    const open = set[1];
+    const high = set[3];
+    const low = set[4];
+    const vol = set[5];
 
-  export default preProcessData;
+    const eachEntry = {
+      ts_code: "000001.SH",
+      trade_date,
+      close,
+      open,
+      high,
+      low,
+      vol,
+    };
+    processedData.push(eachEntry);
+  });
+  return processedData;
+};
+
+export default preProcessData;
